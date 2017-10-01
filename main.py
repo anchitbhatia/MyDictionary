f673013fa2c038bf5c20f7b67c35f81a71e3c511
Anchit Bhatia <anchitbhatia23@gmail.com>
2017-10-02 04:52:24 +0530

    added word similarity

--- main.py
+++ main.py
@@ -6,12 +6,26 @@ def meaning(word):
     if word in data:
         return data[word]
     match = get_close_matches(word, data.keys())
-    return "Do you mean " + str(match)
+    if len(match)>0:
+        cont = input("Do you mean %s ? Enter y/n "%match[0]).lower()
+        if cont =="y":
+            return data[match[0]]
+        elif cont =="n":
+            return "Word doesn't exist !"
+        else:
+            return "Wrong entry !"
+    else:
+        return "Word doesn't exist !"
 
 if __name__ == '__main__':
     data = json.load(open("data.json", "r"))
     while 1:
         w = input("Enter word : ").lower()
-        print( meaning(w))
+        m = meaning(w)
+        if type(m)==list:
+            for i in m:
+                print(i)
+        else:
+            print(m)
         print("------")
 
